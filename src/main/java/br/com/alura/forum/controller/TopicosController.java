97fd889496a6fad1e5f3e9c360ea592469d34353
Dumilde Matos <dumildejosematos@gmail.com>
2021-09-12 18:18:43 +0100

    Adicionar pesquisa simples e com filtros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,20 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
-    @RequestMapping("/topicos")
-    public List<TopicoDTO> Lista() {
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-        Topico topico =
-                new Topico(
-                        "Dúvida",
-                        "Dúvida com spring",
-                        new Curso("Spring", "Programação"));
-        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+    @RequestMapping("/topicos")
+    public List<TopicoDTO> Lista(String nomeCurso) {
+
+        if(nomeCurso == null) {
+            List<Topico> topico = topicoRepository.findAll();
+            return TopicoDTO.converter(topico);
+        }else{
+            List<Topico> topico = topicoRepository.findByCurso_Nome(nomeCurso);
+            return TopicoDTO.converter(topico);
+        }
+
     }
 
 }
